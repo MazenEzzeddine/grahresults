c047db34599c36b0b4f49fdd6c7c994dc1f24c7a
m.ezzeddine <mazen.ezzeddine@gmail.com>
2023-02-05 19:12:26 +0100

    new result IIII

--- data111/data111.py
+++ data111/data111.py
@@ -26,9 +26,8 @@ def graf():
 
     #############iiiiiiitttttttttttttttttttiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiooooooooooooooooooooooooooooooooooooooooooooooooooyyyyyyyyyyyyyyyyyyyyyyyyyykkkkkkkkkkkkkkkkkiiii
 
-    print('hello')
 
-    with open('arissuer171.csv', 'r') as csvfile:
+    with open('arriss111.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             # t.append(math.floor(double(row[0])))
@@ -38,7 +37,8 @@ def graf():
             lamda15.append(double(row[1]))
 
 
-    lr1= [1300, 769, 593, 487, 216, 206, 190, 189,176, 164, 155, 131, 113, 103, 92, 99, 96, 86, 81, 168, 198, 260]
+    #lr1= [1300, 769, 593, 487, 216, 206, 190, 189,176, 164, 155, 131, 113, 103, 92, 99, 96, 86, 81, 168, 198, 260]
+    lr1= [1773, 1573,1080, 830, 849,79.5,67.5,57.3,42.8,197,346,489,634,613,610,591,591,584,565,585,584,579,587,591,618,668,707,743,760,759,758,748,731,722,710,698,690,674,660,643,631,619,607,598,587,576,566,556,546,540,532,524,518,510,503,500,492,487,480,473,467,459,453,444,433,422,662,649,889,1128,855,2341,2072,1803,1794,277,265,252,239,228,215,204,194,184,176,169,160,153,149,143,140]
     lr2= [863,843, 841, 833, 818, 806, 793, 783, 771, 764, 745, 724, 695, 663, 627, 579, 563, 524, 483, 440, 358, 320, 282]
 
     r1end=0
@@ -47,7 +47,8 @@ def graf():
     tr2 = []
     r2start = 0
     r2end=0
-    with open('rissuer171.csv', 'r') as csvfile:
+    print(len(lr1))
+    with open('riss111.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             # t.append(math.floor(double(row[0])))
@@ -55,16 +56,19 @@ def graf():
             t1.append(s1)
 
             lamda1.append(double(row[1]))
-            if double(row[1]) == 350 and r1end!=1:
+            if str(row[0]) == '2023-01-16 13:41:40' :
+                r1start = 1
+            if r1start==1 and r1end !=1:
                 tr1.append(s1)
-                r1start=1
-            if r1start == 1 and double(row[1]) == 175 :
+            if str(row[0]) == '2023-01-16 13:49:10':
                 r1end=1
-            if r1end == 1 and double(row[1]) == 350 and r2end != 1 :
-                r2start = 1
-                tr2.append(s1)
-            if r2start == 1 and double(row[1]) == 175:
-                r2end = 1
+            # if r1start == 1 and double(row[1]) == 525 :
+            #     r1end=1
+            # if r1end == 1 and double(row[1]) == 350 and r2end != 1 :
+            #     r2start = 1
+            #     tr2.append(s1)
+            # if r2start == 1 and double(row[1]) == 175:
+            #     r2end = 1
 
             s1 += 5
     print(tr1)
@@ -72,7 +76,7 @@ def graf():
     t=0
     lt=[]
     lv=[]
-    with open('1l.csv', 'r') as csvfile:
+    with open('latency111.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             # t.append(math.floor(double(row[0])))
@@ -92,7 +96,7 @@ def graf():
     sect = 0
     sec = []
     secta = []
-    with open('arr171sec.csv', 'r') as csvfile:
+    with open('ar111.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             # t.append(math.floor(double(row[0])))
@@ -104,7 +108,7 @@ def graf():
     sectr = 0
     secr = []
     sectar = []
-    with open('r171sec.csv', 'r') as csvfile:
+    with open('r111.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             # t.append(math.floor(double(row[0])))
@@ -116,7 +120,7 @@ def graf():
     acqt = 0
     acq = []
     acqta = []
-    with open('arr171seco.csv', 'r') as csvfile:
+    with open('aracq111.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             # t.append(math.floor(double(row[0])))
@@ -128,7 +132,7 @@ def graf():
     acqtr = 0
     acqr = []
     acqtar = []
-    with open('r171seco.csv', 'r') as csvfile:
+    with open('racq111.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             # t.append(math.floor(double(row[0])))
@@ -161,7 +165,8 @@ def graf():
     #
     ax4.plot(lt, lv, 'r', label='latency (ms)')
     ax4.plot(tr1, lr1, label='latency 2nd replica (ms)')
-    ax4.plot(tr2, lr2, 'g' , label='latency 2nd replica (ms)')
+    #ax4.plot(tr2, lr2, 'g' , label='latency 2nd replica (ms)')
+
 
 
 
